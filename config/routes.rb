ccaf97be76479833618b3345c1f0e23973200809
zakevicius <m.zakevicius@gmail.com>
2021-05-02 22:48:16 +0300

    Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
+#frozen_string_literal: true
+
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :stages, only: %i[index show update] do
+    resources :histories, only: :index
+  end
 end
