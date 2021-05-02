85020acd4e4d63ac99fca8eb14f88dffd6e4ceaf
zakevicius <m.zakevicius@gmail.com>
2021-05-02 19:01:45 +0300

    Set app as api only

--- config/application.rb
+++ config/application.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require_relative "boot"
 
 require "rails/all"
@@ -10,6 +12,7 @@ module StageManager
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
+    config.api_only = true
 
     # Configuration for the application, engines, and railties goes here.
     #
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
@@ -73,4 +75,6 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  config.debug_exception_response_format = :api
 end
