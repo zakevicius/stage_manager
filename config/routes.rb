96b4a4795715b3912e2df6d20eb7b4eaa7ed66ae
zakevicius <m.zakevicius@gmail.com>
2021-05-22 14:55:59 +0300

    add root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
-#frozen_string_literal: true
+# frozen_string_literal: true
 
 Rails.application.routes.draw do
+  root to: 'stages#index'
+
   resources :stages, only: %i[index show update] do
     resources :logs, only: :index
   end
