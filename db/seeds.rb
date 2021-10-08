711f73445159f66f1346d13353a480721080414d
Marius <m.zakevicius@gmail.com>
2021-10-08 18:09:24 +0300

    7 stages

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-5.times do |i|
-  Stage.create({ stage: i + 1, actions: %w[claim unclaim] })
+7.times do |i|
+  Stage.create({ stage_id: i + 1, actions: %w[claim unclaim] })
 end
